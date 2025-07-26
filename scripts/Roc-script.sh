b31a9fcdf673e9508ce6f9c19a568c6f6eca3b67
Newxin394 <q843352112@gmail.com>
2025-07-27 04:06:44 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -32,6 +32,6 @@ git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAp
 git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
-
+git clone --depth=1 https://github.com/sbwml/luci-app-quickfile package/luci-app-quickfile
 ./scripts/feeds update -a
 ./scripts/feeds install -a
